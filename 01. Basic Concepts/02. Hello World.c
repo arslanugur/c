467d08937e563d57aefa5834c4f1c01602fb35f4
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-03-07 17:59:34 +0300

    Create 02. Hello World.c

---
+++ 01. Basic Concepts/02. Hello World.c
@@ -0,0 +1,36 @@
+// Hello World!
+
+// As when learning any new language, the place to start is with the classic "Hello World!" program: 
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+
+// Let's break down the code to understand each line:
+// #include <stdio.h> The function used for generating output is defined in stdio.h. 
+// In order to use the printf function, we need to first include the required file, also called a header file.
+
+// int main() The main() function is the entry point to a program. 
+// Curly brackets { } indicate the beginning and end of a function (also called a code block). 
+// The statements inside the brackets determine what the function does when executed. 
+
+// List of inbuilt C functions in stdio.h file: 
+// printf() This function is used to print the character, string, float, integer, octal and hexadecimal values onto the output screen 
+// scanf() This function is used to read a character, string, numeric data from keyboard. 
+// getc() It reads character from file gets() It reads line from keyboard getchar() 
+// It reads character from keyboard puts() It writes line to o/p screen putchar() It writes a character to screen clearerr() 
+// This function clears the error indicators f open() 
+// All file handling functions are defined in stdio.h header file f close() closes an opened file getw() reads an integer from file 
+// putw() writes an integer to file f 
+// getc() reads a character from file 
+// putc() writes a character to file f 
+// gets() reads string from a file, one line at a time f 
+// puts() writes string to a file f 
+// eof() finds end of file more at https://www.hackerearth.com/practice/notes/why-a-header-file-such-as-includestdioh-is-used/
+
+https://www.sololearn.com/learning/1089/2911/6293/1
+
+s0101earn
+<stdio.h> abbreviation for Standard Input/Output Header std>>Stan
