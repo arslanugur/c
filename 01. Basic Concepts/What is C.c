176d1c7f4f0ee617856cdd1689b7be0a75d6ea6e
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-03-07 17:53:44 +0300

    Update and rename intro-to-swift.swift to 01. Basic Concepts/What is C.c

---
+++ 01. Basic Concepts/What is C.c
@@ -0,0 +1,20 @@
+// Introducing C
+// C - Versatile, and low level language
+// C is a General-Purpose Programming Language that has been around for nearly 50 years.
+// C has been used to write everything from operating systems (including Windows and many others) 
+// to complex programs like the Python interpreter, Git, Oracle database, and more.
+// The versatility of C is by design. It is a low-level language that relates closely to the way machines work while still being easy to learn.
+// *** Understanding how computer memory works is an important aspect of the C programming language. ***
+
+// A document for C programming in Linux: https://linuxconfig.org/c-development-on-linux-introduction-i 
+
+// GENERAL PROGRAMMING LANGUAGES
+// A general-purpose programming language is a programming language designed to be used for writing software in the widest variety of application domains.
+// EXAMPLES of some famous GPLs: 1. C 2. C++ 3. Python 4. Ruby 5. Rust 6. Go 7. Scala 8. C# 9. Swift 10. Java 11. Perl 12. JavaScript 13. PHP 14. BASIC 15. FORTRAN 
+
+// CLIENT-SIDE SCRIPTING LANGUAGE
+// The script can be written in Two form:
+// 01. Server End (Back end):   Server require for processing   >> Relatively Secure  >> PHP, ASP.net, Ruby on Rails, Python etc.
+// 02. Client End (Server end): Browser require for processing  >> Insecure           >> HTML, CSS, JavaScript 
+
+
